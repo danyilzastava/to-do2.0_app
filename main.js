b327a32760aee4804dfe54d5bd79d8b4eecc3432
Danyil Zastava <83227616+danyilzastava@users.noreply.github.com>
2024-11-01 13:58:06 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,55 @@
+const inputBox = document.getElementById("input-box");
+const listContainer = document.getElementById("list-container");
+const emptyMessage = document.querySelector(".empty_list");
+
+
+function checkEmptyList(){
+    if(listContainer.children.length === 0){
+        emptyMessage.style.display = 'block';
+    } else {
+        emptyMessage.style.display = 'none';
+    }
+}
+
+
+function addTask() {
+    if(inputBox.value === ''){
+        alert("You must write something!");
+    } 
+    else {
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+        listContainer.appendChild(li);
+
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+
+    }
+    inputBox.value = "";
+    saveData();
+    checkEmptyList();
+}
+
+
+listContainer.addEventListener('click', function(e){
+    if(e.target.tagName === "LI"){
+        e.target.classList.toggle('checked');
+        saveData();
+    }
+    else if(e.target.tagName === 'SPAN'){
+        e.target.parentElement.remove();
+        saveData();
+        checkEmptyList();
+    }
+}, false);
+
+function saveData(){
+    localStorage.setItem("data", listContainer.innerHTML);
+}
+
+function showTask(){
+    listContainer.innerHTML = localStorage.getItem("data");
+    checkEmptyList();
+}
+showTask();
